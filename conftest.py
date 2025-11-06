51342bacaac3bfd5d03f38bb234071ce0c9d53a4
k.artemenko <kostikkms@gmail.com>
2025-11-06 09:19:28 +0300

    return headless mode

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture()
 def driver():
     options = Options()
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_argument("--window-size=1920,1080")
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
